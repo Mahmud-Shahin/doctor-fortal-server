121026451bde2da2eccf71e4b5ac79d024b55245
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-02-20 09:40:29 +0600

    update data

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// const express = require("express");
+// const cors = require("cors");
+// require("dotenv").config();
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from doctor uncle");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Doctors portal listening on port ${port}`);
+// });
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -6,6 +23,9 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://doctors_administrator:<password>@cluster0.giiqmhh.mongodb.net/?retryWrites=true&w=majority";
 
 app.get("/", (req, res) => {
   res.send("Hello from doctor uncle");
