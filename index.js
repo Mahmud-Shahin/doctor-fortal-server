4167ffe920ca39cbcf466a4e383a28cc0cf66ad3
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-04-29 23:51:06 +0600

    update

--- index.js
+++ index.js
@@ -17,6 +17,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -61,7 +62,12 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
     });
     // warning
     // this is not the proper way to query
