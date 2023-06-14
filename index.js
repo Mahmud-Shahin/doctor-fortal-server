388742c5fc0191c512c6132b90dc0f25b4febab6
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-06-14 20:15:34 +0600

    update data

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
