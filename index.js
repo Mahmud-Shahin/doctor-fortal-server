fa83e0375f37dc92333a344134557f387f0d3144
Mahmudshahin <Shahinmahmud184@gmail.com>
2023-06-15 09:38:18 +0600

    update data

--- index.js
+++ index.js
@@ -75,6 +75,21 @@ async function run() {
       }
     };
 
+    // app.post('/create-payment-intent', verifyJWT async(req,res) =>{
+    app.post('/create-payment-intent', async(req,res) =>{
+      const service = req.body;
+      const price = service.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount ,
+        currency: "usd",
+        payment_method_types:['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret});
+      
+      
+    })
+
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query).project({ name: 1 });
